76257bb49350c301369647870c6e090b8927e41b
Sai Narendra <tannirusainarendra81@gmail.com>
2025-08-28 13:36:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	 if (typeof make == "string" || typeof model == "string") {
+    console.log("Eroor");
+  }
+  this.make = make;
+  this.model = model;
+}
+Car.prototype.getMakeModel = function () {
+  return `${this.make} ${this.model}`;
+};
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	  this.make = make;
+  this.model = model;
+  this.topSpeed = topSpeed;
+}
+
+SportCar.prototype = Object.create(Car.prototype);
+
+SportCar.prototype.getTopSpeed = function () {
+  return this.topSpeed;
+};
+
+const car = new SportCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel());
+console.log(car.getTopSpeed());
+
 
 // Do not change the code below
 window.Car = Car;
